ce26a40caae4c62ffa0b40e6a06b459cfac2df40
Fredy Rogez <fredy@rogez.net>
2020-08-25 16:07:52 +0200

    update 25/08/2020

---
+++ README.md
@@ -0,0 +1,10 @@
+# SDL C VS Template (MAJ : 25/08/2020)
+Solution Visual Studio 2019 pour démarrer rapidement un projet C/SDL2 : linkage, sous-systèmes, includes, libs, dll, configs debug/release, x86/x64, gitignore...
+
+Mise à jour des libs vers les versions les plus récentes le 25 août 2020.
+
+* SDL 2.0.12
+* SDL_image 2.0.5
+* SDL_ttf 2.0.15
+* SDL_mixer 2.0.4
+* SDL_net 2.0.1
---
+++ main.c
@@ -0,0 +1,89 @@
+/*
+    SDL C VS Template
+    Copyright (c) 2019-2020 Fredy Rogez (rogez.net)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#include <stdio.h>
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"
+#include "SDL_net.h"
+#include "SDL_ttf.h"
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+int main(int argc, char* args[])
+{
+    int game_running=1;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_RENDERER_PRESENTVSYNC);
+    TTF_Init();
+    
+    SDL_Window* window = SDL_CreateWindow("my game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        game_running = 0;
+        printf("ERROR : SDL_CreateWindow");
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        game_running = 0;
+        printf("ERROR : SDL_CreateRenderer");
+    }
+
+    SDL_Surface* surface = IMG_Load("assets/texture.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    
+    while (game_running)
+    {
+        // Events
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT: // Clic sur la croix
+                game_running = 0;
+                break;
+            }
+        }
+
+        // Update
+
+
+        // Draw
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+        
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+    return 0;
+}
